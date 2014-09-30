7c69c8c251d31c7f7f6aadeca62da37a9827d1a3
Jason Felice <jason.m.felice@gmail.com>
2014-09-30 12:47:01 -0400

    Extract impromptu.scatter

--- impromptu.scm
+++ impromptu.scm
@@ -1,38 +1,9 @@
+(include "impromptu.scatter.scm")
+
 (module impromptu (edit-properties)
 
   (import scheme chicken)
-  (use posix irregex embedded-test utils)
-
-  ;; -- Generating templates --
-
-  (define (format-value value)
-    (if (memv #\newline (string->list value))
-      (string-append "\n" value)
-      (string-append " " value)))
-
-  (define (format-entry entry)
-    (let ((name (car entry))
-	  (value (cdr entry)))
-      (string-append
-	":" (symbol->string name) ":"
-	(format-value value))))
-
-  (define (make-template alist)
-    (if (null? alist)
-      ""
-      (string-append
-	(format-entry (car alist))
-	"\n"
-	(make-template (cdr alist)))))
-
-  (test (make-template '())
-	"")
-  (test (make-template '((Foo: . "bar")))
-	":Foo: bar\n")
-  (test (make-template '((Foo: . "bar") (Baz: . "quux")))
-	":Foo: bar\n:Baz: quux\n")
-  (test (make-template '((Foo: . "bar") (Baz: . "Hello, this is a\nmulti-line test.")))
-	":Foo: bar\n:Baz:\nHello, this is a\nmulti-line test.\n")
+  (use posix irregex embedded-test utils impromptu.scatter)
 
   ;; -- Parsing templates --
 
